c5b9c07163c54a72c9ab29f9a1fb7b54e6e3ef9b
Bhavya <109464004+BhavyaGamer7@users.noreply.github.com>
2024-06-25 11:44:09 +0530

    The new Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.gradle)
+        classpath(libs.kotlin.gradle.plugin)
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
